b321ea9e18f937062f54c52e5bb73519c37a7bbf
bit <bit@DESKTOP-518UE4O>
2018-02-23 14:03:57 +0900

    prob04 solved

--- src/prob04/Person.java
+++ src/prob04/Person.java
@@ -5,10 +5,22 @@ public class Person {
 	private int age;
 	private String name;
 	
+	public Person() {
+		age = 12;
+		name = "";
+		numberOfPerson++;
+	}
+	
 	public Person( String name ){
+		this.age = 12;
+		this.name = name;
+		numberOfPerson++;
 	}
 	
 	public Person(int age, String name){
+		this.age = age;
+		this.name = name;
+		numberOfPerson++;
 	}
 	
 	public int getAge() {
@@ -24,12 +36,11 @@ public class Person {
 		this.name = name;
 	}
 	public void selfIntroduce(){
-		// "내 이름은 ...이며, 나이는 ...살입니다." 출력함
-		// 예) 내이름은 김철수이며, 나이는 19살 입니다.
+		System.out.println("내 이름은 " + name + "이며, 나이는 " + age + "살 입니다.");
 	}
 
 	static int getPopulation(){
-		// 전체 인구수를 return 함
+		return Person.numberOfPerson;
 	}
 }
 
